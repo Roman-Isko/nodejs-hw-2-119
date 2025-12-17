22bb9997506ed241b3cccfa2e4190083fe1a8fe9
Roman-Isko <roman_isko@ukr.net>
2025-12-17 12:53:34 +0200

    add initials files

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,20 @@
+// src/middleware/errorHandler.js
+export const errorHandler = (err, req, res, next) => {
+  const status = err.status || 500;
+
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(status).json({
+    message: err.message,
+  });
+};
+
+// export const errorHandler = (err, req, res, next) => {
+//   const status = err.status || 500;
+
+//   res.status(status).json({
+//     message: err.message,
+//   });
+// };
