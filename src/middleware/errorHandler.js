5ea89b5b55c2fb160081a61d1000259466778f8b
Roman-Isko <roman_isko@ukr.net>
2025-12-17 14:10:41 +0200

    add intials files

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -11,6 +11,8 @@ export const errorHandler = (err, req, res, next) => {
   });
 };
 
+//////////////////////////////////////////
+
 // export const errorHandler = (err, req, res, next) => {
 //   const status = err.status || 500;
 
